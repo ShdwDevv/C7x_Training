65b35670fd320b3eab1a618efcf3bf020c312a97
shdwdevv <arshathahamed500@gmail.com>
2025-01-16 11:32:47 +0530

    Finished the total sum problem

---
+++ simd_programs/problems.md
@@ -0,0 +1,10 @@
+## Total Sum Calculation
+### PSEUDO CODE
+1. Iniliatize the maximum value as 1024
+1. Create a 2 vector with initialized as 8 and 0
+1. create an another vector contain sequence of number from 1-8
+1. Do add operation between sum and value vector , value and step vector and icnrement the loop by 8
+1. Store the sum in result array and add every value to get eh output
+* Output
+
+![alt text](image-1.png)
---
+++ simd_programs/total_calculation.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "immintrin.h"
+using namespace std;
+#define MAX_NUM (2048)
+int normal_addition(){
+    int sum = 0, iterations = 0;
+    for(int val = 1;val <= MAX_NUM;val++){
+        sum += val;
+        iterations++;
+    }
+    cout<<"Total : "<<sum<<" Iterations : "<<iterations<<endl;
+    return sum;
+}
+int intrinsic_function(){
+    int sum = 0, iterations = 0;
+    __m256i eight_step = _mm256_set1_epi32(8);
+    __m256i sum_vector = _mm256_set1_epi32(0);
+    int values[8] = {1,2,3,4,5,6,7,8};
+    __m256i value_vector = _mm256_loadu_si256((__m256i*)values);
+    for(int idx = 0;idx < MAX_NUM;idx+=8){
+        iterations++;
+        sum_vector = _mm256_add_epi32(sum_vector,value_vector);
+        value_vector = _mm256_add_epi32(value_vector,eight_step);
+    }
+    int res[8];
+    _mm256_storeu_si256((__m256i*)&res,sum_vector);
+    for(auto val : res) sum += val;
+    cout<<endl;
+    cout<<"Total : "<<sum<<" Iterations : "<<iterations<<endl;
+    return sum;
+}
+int main(){
+    int res1 = normal_addition();
+    int res2 = intrinsic_function();
+}
